a42572cf5007838deebb28e8fe6e5784b3520c6b
Viktoria_Atavina <114982093+viktoriaAtavina@users.noreply.github.com>
2023-04-02 21:13:36 +0300

    Create 2.java

---
+++ 2.java
@@ -0,0 +1,32 @@
+/**
+ * Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
+ */
+
+package java_homework.hw4;
+
+import java.util.ArrayDeque;
+
+public class Task2 {
+    public static ArrayDeque<Integer> getReversDeque(int count) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        ArrayDeque<Integer> reverseDeque = new ArrayDeque<>();
+
+        for (int i = 0; i < count; i++) {
+            int rand = (int) (Math.random() * (100 - 1));
+            deque.addFirst(rand);
+        }
+        System.out.println(deque);
+
+        for (int i = 0; i < count; i++) {
+            reverseDeque.addFirst(deque.getFirst());
+            deque.removeFirst();
+        }
+        return reverseDeque;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(getReversDeque(10));
+
+    }
+}
